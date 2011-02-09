132a591d23f79f22111a3aeb6cff6f571cb55574
gUstehn <gustehn@gUstehn.(none)>
2011-02-09 11:06:15 +0100

    Added showing current position files Demo.java and Main.java

---
+++ Demo.java
@@ -0,0 +1,138 @@
+package org.openstreetmap.gui.jmapviewer;
+
+//License: GPL. Copyright 2008 by Jan Peter Stotz
+
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
+
+/**
+ *
+ * Demonstrates the usage of {@link JMapViewer}
+ *
+ * @author Jan Peter Stotz
+ *
+ */
+public class Demo extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    public static int counter = 0;
+    public Demo() {
+        super("JMapViewer Demo");
+        setSize(400, 400);
+        final JMapViewer map = new JMapViewer();
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        JPanel panel = new JPanel();
+        JPanel helpPanel = new JPanel();
+        add(panel, BorderLayout.NORTH);
+        add(helpPanel, BorderLayout.SOUTH);
+        JLabel helpLabel = new JLabel("Use right mouse button to move,\n "
+                + "left double click or mouse wheel to zoom.");
+        helpPanel.add(helpLabel);
+        JButton button = new JButton("setDisplayToFitMapMarkers");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                map.setDisplayToFitMapMarkers();
+            }
+        });
+        JComboBox tileSourceSelector = new JComboBox(new TileSource[] { new OsmTileSource.Mapnik(),
+                new OsmTileSource.TilesAtHome(), new OsmTileSource.CycleMap() });
+        tileSourceSelector.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                map.setTileSource((TileSource) e.getItem());
+            }
+        });
+        JComboBox tileLoaderSelector;
+        try {
+            tileLoaderSelector = new JComboBox(new TileLoader[] { new OsmFileCacheTileLoader(map),
+                    new OsmTileLoader(map) });
+        } catch (IOException e) {
+            tileLoaderSelector = new JComboBox(new TileLoader[] { new OsmTileLoader(map) });
+        }
+        tileLoaderSelector.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                map.setTileLoader((TileLoader) e.getItem());
+            }
+        });
+        map.setTileLoader((TileLoader) tileLoaderSelector.getSelectedItem());
+        panel.add(tileSourceSelector);
+        panel.add(tileLoaderSelector);
+        final JCheckBox showMapMarker = new JCheckBox("Map markers visible");
+        showMapMarker.setSelected(map.getMapMarkersVisible());
+        showMapMarker.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                map.setMapMarkerVisible(showMapMarker.isSelected());
+            }
+        });
+        panel.add(showMapMarker);
+        final JCheckBox showTileGrid = new JCheckBox("Tile grid visible");
+        showTileGrid.setSelected(map.isTileGridVisible());
+        showTileGrid.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                map.setTileGridVisible(showTileGrid.isSelected());
+            }
+        });
+        panel.add(showTileGrid);
+        final JCheckBox showZoomControls = new JCheckBox("Show zoom controls");
+        showZoomControls.setSelected(map.getZoomContolsVisible());
+        showZoomControls.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                map.setZoomContolsVisible(showZoomControls.isSelected());
+            }
+        });
+        panel.add(showZoomControls);
+        panel.add(button);
+        add(map, BorderLayout.CENTER);
+
+        //
+        /**
+         * Fredrik Gustafsson
+         */
+        JButton current = new JButton("Show current location");
+        panel.add(current);
+       
+        current.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                
+            }
+        });
+        Main main = new Main();
+        main.main(null);
+        counter++;
+        try {
+            Thread.sleep(5000);
+            map.addMapMarker(new MapMarkerDot(main.getLatitude(), main.getLongitude()));
+            System.out.println("!!!!!!!" + main.getLatitude());
+            System.out.println("!!!!!" + main.getLongitude());
+        } catch (InterruptedException es) {
+            // TODO Auto-generated catch block
+            es.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Demo().setVisible(true);
+    }
+
+}
